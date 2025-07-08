bdfa9f0ef2e8eacfa13d4f66dc8102723d5db529
Tabassum Shaik <140260196+ShaikTabassum1431@users.noreply.github.com>
2025-07-08 15:13:01 +0530

    Update urls.py

--- careers/urls.py
+++ careers/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('job/<int:job_id>/', views.job_detail, name='job_detail'),
+    path('api/search', views.search_jobs, name='search_jobs'),
+
 ]
